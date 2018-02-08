c262dc982f0fcd62ef0876268a1a75b88a855b93
Maxim Kulkin <maxim.kulkin@gmail.com>
2018-02-08 09:43:20 -0800

    Update makefiles to support ESP_IDF SDK

--- component.mk
+++ component.mk
@@ -1,52 +1,65 @@
 # Component makefile for homekit
-ifndef wolfssl_ROOT
-$(error Please include wolfssl component prior to homekit)
-endif
 
-ifndef cJSON_ROOT
-$(error Please include cJSON component prior to homekit)
-endif
+ifdef component_compile_rules
+    # ESP_OPEN_RTOS
+    ifndef wolfssl_ROOT
+    $(error Please include wolfssl component prior to homekit)
+    endif
 
-ifndef http-parser_ROOT
-$(error Please include http-parser component prior to homekit)
-endif
+    ifndef cJSON_ROOT
+    $(error Please include cJSON component prior to homekit)
+    endif
 
-# Base flash address where persisted information (e.g. pairings) will be stored
-HOMEKIT_SPI_FLASH_BASE_ADDR ?= 0x100000
-# Maximum number of simultaneous clients allowed.
-# Each connected client requires ~1100-1200 bytes of RAM.
-HOMEKIT_MAX_CLIENTS ?= 16
+    ifndef http-parser_ROOT
+    $(error Please include http-parser component prior to homekit)
+    endif
 
-INC_DIRS += $(homekit_ROOT)/include
+    # Base flash address where persisted information (e.g. pairings) will be stored
+    HOMEKIT_SPI_FLASH_BASE_ADDR ?= 0x100000
+    # Maximum number of simultaneous clients allowed.
+    # Each connected client requires ~1100-1200 bytes of RAM.
+    HOMEKIT_MAX_CLIENTS ?= 16
 
-homekit_INC_DIR = $(homekit_ROOT)/include $(homekit_ROOT)/src
-homekit_SRC_DIR = $(homekit_ROOT)/src
+    INC_DIRS += $(homekit_ROOT)/include
 
-$(eval $(call component_compile_rules,homekit))
+    homekit_INC_DIR = $(homekit_ROOT)/include $(homekit_ROOT)/src
+    homekit_SRC_DIR = $(homekit_ROOT)/src
 
-EXTRA_WOLFSSL_CFLAGS = \
-	-DWOLFCRYPT_HAVE_SRP \
-	-DWOLFSSL_SHA512 \
-	-DNO_MD5 \
-	-DNO_SHA \
-	-DHAVE_HKDF \
-	-DHAVE_CHACHA \
-	-DHAVE_POLY1305 \
-	-DHAVE_ED25519 \
-	-DHAVE_CURVE25519 \
-	-DNO_SESSION_CACHE \
-	-DRSA_LOW_MEM \
-	-DGCM_SMALL \
-	-DCURVE25519_SMALL \
-	-DED25519_SMALL \
-	-DUSE_SLOW_SHA512 \
-	-DWOLFCRYPT_ONLY
+    $(eval $(call component_compile_rules,homekit))
 
-wolfssl_CFLAGS += $(EXTRA_WOLFSSL_CFLAGS)
-homekit_CFLAGS += $(EXTRA_WOLFSSL_CFLAGS) \
-	-DSPIFLASH_BASE_ADDR=$(HOMEKIT_SPI_FLASH_BASE_ADDR) \
-	-DHOMEKIT_MAX_CLIENTS=$(HOMEKIT_MAX_CLIENTS)
+    EXTRA_WOLFSSL_CFLAGS = \
+        -DWOLFCRYPT_HAVE_SRP \
+        -DWOLFSSL_SHA512 \
+        -DNO_MD5 \
+        -DNO_SHA \
+        -DHAVE_HKDF \
+        -DHAVE_CHACHA \
+        -DHAVE_POLY1305 \
+        -DHAVE_ED25519 \
+        -DHAVE_CURVE25519 \
+        -DNO_SESSION_CACHE \
+        -DRSA_LOW_MEM \
+        -DGCM_SMALL \
+        -DCURVE25519_SMALL \
+        -DED25519_SMALL \
+        -DUSE_SLOW_SHA512 \
+        -DWOLFCRYPT_ONLY
 
-ifeq ($(HOMEKIT_DEBUG),1)
-homekit_CFLAGS += -DHOMEKIT_DEBUG
+    wolfssl_CFLAGS += $(EXTRA_WOLFSSL_CFLAGS)
+    homekit_CFLAGS += $(EXTRA_WOLFSSL_CFLAGS) \
+        -DESP_OPEN_RTOS \
+        -DSPIFLASH_BASE_ADDR=$(HOMEKIT_SPI_FLASH_BASE_ADDR) \
+        -DHOMEKIT_MAX_CLIENTS=$(HOMEKIT_MAX_CLIENTS)
+
+    ifeq ($(HOMEKIT_DEBUG),1)
+    homekit_CFLAGS += -DHOMEKIT_DEBUG
+    endif
+
+else
+    # ESP_IDF
+    COMPONENT_DEPENDS = esp-wolfssl esp-cjson esp-http-parser
+
+    COMPONENT_PRIV_INCLUDEDIRS = src
+    COMPONENT_SRCDIRS = src
+
 endif
